f53b86cb228f022ec94b9cfc458dafbceec68707
Pranjali <pranjalidas2201@gmail.com>
2025-04-05 15:00:10 -0700

    donation model

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,13 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
 
+app.use('/api/donations', donationRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
